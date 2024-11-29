435aebd4cdb10fae77a180e5e5d3529ef58a9788
Raymond Porrata <rporrata@gmail.com>
2024-11-29 09:08:58 -0500

    Update script.js
    Spelling error tow vs two.

--- demo/script.js
+++ demo/script.js
@@ -2,7 +2,7 @@ $(function () {
     var dialogsManager = new DialogsManager.Instance(),
         dialog = dialogsManager.createWidget( 'confirm', {
             headerMessage: 'Hello World',
-            message: 'I\'m a pretty simple confirm dialog with tow buttons and a background overlay.',
+            message: 'I\'m a pretty simple confirm dialog with two buttons and a background overlay.',
             closeButton: true
         } );
 
